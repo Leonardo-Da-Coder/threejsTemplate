fec0f13a0cc57a4a11c9b666ea6d84a2b00fd4f1
Leon Trunk <leontrunk@gmx.de>
2026-04-21 14:23:54 +0200

    change: added leva

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@
 import { Canvas } from "@react-three/fiber"
 import {Stage} from "@react-three/drei"
 import { Experience } from "./components/Experience"
+import { useControls } from "leva"
 
 function App() {
+  const {color} = useControls({color: '#6c6eb4'})
   return (
+  <div className="container" style={{background: color}}>
   <Canvas shadows camera={{position: [4,0,-12], fov:35}}>
     <Stage>
-      <Experience></Experience>
+      <Experience/>
     </Stage>
   </Canvas>
+  </div>
   )
 }
 
