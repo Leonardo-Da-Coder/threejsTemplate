d86bfaf047518dc06dc3126ad3b16ed651b70a84
Leon Trunk <leontrunk@gmx.de>
2026-04-22 17:14:40 +0200

    refactor: removed import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import { Canvas } from "@react-three/fiber"
 import { Experience } from "./components/Experience"
 import { useControls } from "leva"
-import { Stage } from "@react-three/drei"
 import { Suspense } from "react"
 
 function App() {
